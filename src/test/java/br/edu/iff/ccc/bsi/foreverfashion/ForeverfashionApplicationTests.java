fe4c574b967868ec1eced4febffae942cd16110e
Carlos Henrique Tito Azeredo <58192819+carlostitoaz@users.noreply.github.com>
2025-02-16 13:04:22 -0300

    add estrutura do projeto

---
+++ src/test/java/br/edu/iff/ccc/bsi/foreverfashion/ForeverfashionApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.iff.ccc.bsi.foreverfashion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForeverfashionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
